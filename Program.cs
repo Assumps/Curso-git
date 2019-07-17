3f75052ef05bea0c062cf91166a7d752326b539a
Assumps <jrvitor5@gmail.com>
2019-07-17 11:19:33 -0300

    acrescimo de um laço de repeticao com mensagem

--- Program.cs
+++ Program.cs
@@ -3,7 +3,14 @@
 namespace Projeto_git {
     class Program {
         static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine("bom dia");
+
+            for (int i = 0; i < n; i++){
+                Console.WriteLine("Trab");
+            }
 
             Console.ReadLine();
         }
